05b2ae6b49946825bae940a7f0c16b8a84ebfe40
Aleksei Emeliantsev <aleksei.emeliantsev@abbyy.com>
2023-08-21 14:20:17 +0300

    Finished chapter 4

---
+++ chapter04/example_4.1/listing_4.1.cpp
@@ -0,0 +1,87 @@
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <queue>
+#include <iostream>
+
+bool more_data_to_prepare()
+{
+    static int count = 0;
+
+    if( count++ < 10 ) {
+
+        return true;
+    }
+
+    return false;
+}
+
+struct data_chunk {} ;
+
+data_chunk prepare_data()
+{
+    return data_chunk();
+}
+
+void process( data_chunk& )
+{
+    std::cout << "Process" << std::endl;
+
+}
+
+bool is_last_chunk( data_chunk& )
+{
+    static int count = 0;
+
+    if( count++ < 9 ) {
+
+        return false;
+    }
+
+    return true;
+}
+
+std::mutex mut;
+std::queue<data_chunk> data_queue;
+std::condition_variable data_cond;
+
+void data_preparation_thread()
+{
+    while( more_data_to_prepare() ) {
+        
+        data_chunk const data = prepare_data();
+        std::lock_guard<std::mutex> lk( mut );
+        data_queue.push( data );
+        data_cond.notify_one();
+    }
+}
+
+void data_processing_thread()
+{
+    while( true ) {
+
+        std::unique_lock<std::mutex> lk( mut );
+        
+        data_cond.wait( lk, [] { return !data_queue.empty(); } );
+        
+        data_chunk data = data_queue.front();
+
+        data_queue.pop();
+        
+        lk.unlock();
+        
+        process( data );
+        
+        if( is_last_chunk( data ) )
+            break;
+    }
+}
+
+int main()
+{
+    std::thread t1( data_preparation_thread );
+    std::thread t2( data_processing_thread );
+
+    t1.join();
+    t2.join();
+}
---
+++ chapter04/example_4.13/listing_4.13.cpp
@@ -0,0 +1,49 @@
+#include <list>
+#include <algorithm>
+#include <future>
+#include <cassert>
+
+template<typename T>
+std::list<T> parallel_quick_sort( std::list<T> input )
+{
+    if( input.empty() ) {
+        return input;
+    }
+
+    std::list<T> result;
+    result.splice( result.begin(), input, input.begin() );
+
+    T const& pivot = *result.begin();
+
+    auto divide_point = std::partition( input.begin(), input.end(),
+        [&]( T const& t ) { return t < pivot; } );
+
+    std::list<T> lower_part;
+
+    lower_part.splice( lower_part.end(), input, input.begin(),
+        divide_point );
+
+    std::future<std::list<T> > new_lower(
+        std::async( &parallel_quick_sort<T>, std::move( lower_part ) ) );
+
+    auto new_higher(
+        parallel_quick_sort( std::move( input ) ) );
+
+    result.splice( result.end(), new_higher );
+    result.splice( result.begin(), new_lower.get() );
+
+    return result;
+}
+
+
+int main()
+{
+    std::list<int> nums = { 8,4,5,12,3,7,5,6, 0 };
+
+    auto sorted_nums = parallel_quick_sort( nums );
+
+    assert( std::is_sorted( sorted_nums.begin(), sorted_nums.end() ) );
+
+
+    return 0;
+}
